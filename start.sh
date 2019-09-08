e95330782cc871691b78621cafd69a8bc81789a0
Christopher Fujino <christopherfujino@gmail.com>
2019-09-08 16:24:56 -0700

    init commit

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker run \
+  -it \
+  --rm \
+  --volume="$PWD:/myapp" \
+  --volume="$PWD/vendor:/usr/local/bundle" \
+  -p 3000:3000 \
+  christopherfujino/peas-budget:latest
