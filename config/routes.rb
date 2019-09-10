8f853c58aee12e3ed63e6ae9297a7979809f1eba
Christopher Fujino <christopherfujino@gmail.com>
2019-09-09 20:28:43 -0700

    port welcome page to slim

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'website/index'
+  root to: 'website#index'
+  #get 'website/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
