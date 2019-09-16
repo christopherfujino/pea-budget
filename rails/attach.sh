dc2c7b5073296702e9f0e597c628e30076f64d70
Christopher Fujino <christopherfujino@gmail.com>
2019-09-15 19:47:11 -0700

    fix grep in attach.sh

--- rails/attach.sh
+++ rails/attach.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CONTAINER_NAME=$(docker container ls --format "{{.Names}}" | grep '^peas-budget_web')
+CONTAINER_NAME=$(docker container ls --format "{{.Names}}" | grep '^pea-budget_web')
 
 docker attach "$CONTAINER_NAME"
