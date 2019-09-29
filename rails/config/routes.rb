d7a20b72b711b817d01cb60e5e493a62111fe64b
Christopher Fujino <christopherfujino@gmail.com>
2019-09-28 21:21:24 -0700

    add accounts routes

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root to: 'website#index'
-  #get 'website/index'
+
+  post 'accounts/create'
+  get 'accounts', to: 'accounts#index'
+  get 'accounts/:account_id', to: 'accounts#show', as: 'accounts_show'
+  patch 'accounts/update'
+  delete 'accounts/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
