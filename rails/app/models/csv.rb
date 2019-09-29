6d0cb0c00f1a0dcbf3167a543aa2fe0906931705
Christopher Fujino <christopherfujino@gmail.com>
2019-09-29 11:34:58 -0700

    add csv model logic to parse csv file

--- rails/app/models/csv.rb
+++ rails/app/models/csv.rb
@@ -1,4 +1,20 @@
 class Csv < ApplicationRecord
   belongs_to :account
   has_one_attached :csv_file
+
+  after_create :process_transactions
+
+  def matrix
+    return @matrix if @matrix.present?
+    @matrix = CSV.parse(csv_file.download)
+  end
+
+  def process_transactions
+    matrix.each do |transaction|
+      transaction.each do |item|
+        puts item
+      end
+    end
+    true
+  end
 end
